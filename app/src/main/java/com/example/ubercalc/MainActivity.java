df7a21fb2990995832d7918c2110f0e79a08463e
jw0922547 <jbyrdab@gmail.com>
2023-03-06 10:32:58 -0600

    changes 1

--- app/src/main/java/com/example/ubercalc/MainActivity.java
+++ app/src/main/java/com/example/ubercalc/MainActivity.java
@@ -2,13 +2,76 @@ package com.example.ubercalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    //put an override before a new function is made
+    int totalMiles = 0;
+    int totalPrice;
+    int mileRate;
+    private Spinner selectedCar;
+    private EditText mileTotaltext;
+    private String finalPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btnClick);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                selectedCar = findViewById(R.id.vehicleDrop);
+                String selectedCarText = selectedCar.getSelectedItem().toString();
+                mileTotaltext = findViewById(R.id.mileNum);
+                totalMiles = Integer.parseInt (mileTotaltext.getText().toString());
+                // checks the value and adjusts the extra fee
+                if (selectedCarText == "Sedan"){
+                    mileRate = totalMiles;
+
+                } else if (selectedCarText == "Smart Car") {
+                    mileRate = totalMiles + 2;
+
+                } else if (selectedCarText == "MiniVan") {
+                    mileRate = totalMiles + 5;
+
+                }
+                totalPrice = (int) Math.ceil( mileRate * 3.25 + 3 );
+
+                 finalPrice = (totalPrice.toString());
+                Intent intent = new Intent (MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+        //variable name variable value is a key value pair
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        //creates editor
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        //applies edits to value
+        editor.putString("myString", "pingas");
+        editor.apply();
+    }
+
+
+    class HandleTheClick implements View.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+
+            //Toast.maketext(MainActivity.this, "I am a toast message", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent (MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/ubercalc/SecondActivity.java
+++ app/src/main/java/com/example/ubercalc/SecondActivity.java
@@ -2,7 +2,10 @@ package com.example.ubercalc;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        // applies the string
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        String myString = sharedPreferences.getString("myString", "");
+        //if it works the toast will display it, if not it will be blank
+        Toast.makeText(this, myString, Toast.LENGTH_LONG).show();
+
+        TextView textView = (TextView) findViewById(R.id.mileResult);
+        textView.setText(myString);
     }
 }
